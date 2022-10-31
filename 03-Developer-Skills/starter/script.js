c76a2486c8136d7d404aa447a7160813539a0651
jbettmann <jordan.bettmann@gmail.com>
2022-10-31 14:05:25 -0600

    learned some debugging

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,30 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const measureK = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    // C) fix
+    value: Number(prompt("Degrees celsius:")),
+  };
+
+  console.log(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+// A) identify value
+// console.log(measureK());
+
+const temps1 = [17, 21, 23];
+
+const printForcast = (arr) => {
+  let tempString = ``;
+  for (let i = 0; i < arr.length; i++) {
+    tempString += `It will be ${arr[i]} degrees C in ${i + 1} days. `;
+  }
+  return tempString;
+};
+
+console.log(printForcast(temps1));
