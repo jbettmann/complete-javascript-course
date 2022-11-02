0f950832d28cb8a1916d9f1ae93067ced0109c82
jbettmann <jordan.bettmann@gmail.com>
2022-11-02 16:55:18 -0600

    made modal from sctach

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const showModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const handleOpen = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const handleClose = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+showModal.forEach(modal => modal.addEventListener('click', handleOpen));
+btnCloseModal.addEventListener('click', handleClose);
+overlay.addEventListener('click', handleClose);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    handleClose();
+  }
+});
